adfdb9a40440684a3a455b0440b52700ced3f23e
Laury <61024352+Laury-THIALLIER@users.noreply.github.com>
2023-01-18 19:51:06 +0100

    Ajout des Event Listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,27 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+document.getElementById("arrow_left").addEventListener("click", () => {
+  console.log("Flèche gauche");
+});
+
+document.getElementById("arrow_right").addEventListener("click", () => {
+  console.log("Flèche droite");
+});
